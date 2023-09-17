e93e71b9ffcdbfd7bca0ae4c9fc04bae71f63b6a
haseeb <haseeb.ahmed@deutics.com>
2023-09-17 13:40:06 +0500

    date fixes for hive

--- bcp_data_server/hive/fetcher.py
+++ bcp_data_server/hive/fetcher.py
@@ -3,19 +3,25 @@ from ..fetcher import DataFetcherMain
 
 class DataFetcher:
     class Profile(DataFetcherMain):
+        _dates = ['admission_date', 'left_date', 'last_present_date']
         _endpoint = "/profile"
 
     class EmployeeRole(DataFetcherMain):
+        _dates = []
         _endpoint = "/employee_roles"
 
     class EmployeeProfile(DataFetcherMain):
+        _dates = []
         _endpoint = "/employees"
 
     class Branch(DataFetcherMain):
+        _dates = []
         _endpoint = "/branches"
 
     class Section(DataFetcherMain):
+        _dates = []
         _endpoint = "/sections"
 
     class TeacherGroups(DataFetcherMain):
+        _dates = []
         _endpoint = "/teacher_groups"
