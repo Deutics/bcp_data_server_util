0cac977ddb9dcbf2458f7bb2259325bc9d83dc8a
Peterlight Faboyede <petlight49@gmail.com>
2024-08-26 08:23:46 +0100

    feat(hive) teacher list by branch

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
 
     # Project version number:
-    version='1.0.20',
+    version='1.0.21',
 
     # List a license for the project, eg. MIT License
     license='MIT',
